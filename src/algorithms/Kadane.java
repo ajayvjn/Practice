f1b8d3b0831420afeda983e9164eb2a052a7e6f9
AJ <ajay.vjn17@gmail.com>
2019-11-18 22:59:22 -0700

    add max subarray with index

--- src/algorithms/Kadane.java
+++ src/algorithms/Kadane.java
@@ -3,9 +3,9 @@ package algorithms;
 /**
  * Kadane's Algorithm
  * Maximum Sum Subarray Problem - O(n)
- *
+ * <p>
  * This algorithm handles all cases including all negative numbers in an array.
- *
+ * <p>
  * Created by Ajay on 2/24/17.
  */
 public class Kadane {
@@ -15,6 +15,8 @@ public class Kadane {
 
         // Max Sum Subarray without index
         getMaxSubarray(arr);
+        // Max Sum Subarray with index
+        getMaxSubarrayWithIndex(arr);
     }
 
     private static void getMaxSubarray(int[] arr) {
@@ -27,5 +29,29 @@ public class Kadane {
             maxSA = Math.max(maxSA, maxSATemp);
         }
         System.out.println("Max Sum Subarray: " + maxSA);
+    }
+
+
+    private static void getMaxSubarrayWithIndex(int[] arr) {
+        int maxSA = arr[0];
+        int maxSATemp = arr[0];
+
+        int startIndex = 0;
+        int endIndex = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (maxSATemp + arr[i] < arr[i]) {
+                endIndex = i;
+                startIndex = i;
+            }
+            maxSATemp = Math.max(maxSATemp + arr[i], arr[i]);
+            if (maxSA < maxSATemp) {
+                endIndex = i;
+            }
+            maxSA = Math.max(maxSA, maxSATemp);
+        }
+        System.out.println("Max Sum Subarray: " + maxSA);
+        System.out.println("startIndex: " + startIndex);
+        System.out.println("endIndex: " + endIndex);
     }
 }
